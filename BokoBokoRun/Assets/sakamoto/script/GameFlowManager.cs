50e672282de51e7bc594c06e90b335bf811ff068
13ko13 <208754810+13ko13@users.noreply.github.com>
2026-02-25 12:19:06 +0900

    タイトルシーンからインゲームに遷移する仕組みを作成した

--- BokoBokoRun/Assets/sakamoto/script/GameFlowManager.cs
+++ BokoBokoRun/Assets/sakamoto/script/GameFlowManager.cs
@@ -10,7 +10,7 @@ public sealed class GameFlowManager : MonoBehaviour
     [SerializeField] private TitleManager m_titleManger;   //タイトルマネージャー
     [SerializeField] private InGameManager m_inGameManger;//インゲームマネージャー
     [SerializeField] private ResultManager m_resultManger;//リザルトマネージャー
-
+    [SerializeField] private FadeManager m_fadeManager;//フェードマネージャー
 
     public enum Scene
     {
@@ -28,8 +28,16 @@ public sealed class GameFlowManager : MonoBehaviour
         m_inGameManger.enabled = false;
         m_resultManger.enabled = false;
 
-        //初期シーンをタイトルに設定
+        //タイトルを出す前に黒→透明とする
+        StartCoroutine(BootTitle());
+    }
+
+    private IEnumerator BootTitle()
+    {
+        //シーンをタイトルに切り替える
         ChangeScene(Scene.Title);
+        //フェードを行う
+        yield return m_fadeManager.FadeIn(1.0f, 0.0f);
     }
 
     private void Awake()
@@ -49,43 +57,51 @@ public sealed class GameFlowManager : MonoBehaviour
     {
         switch (m_scene)
         {
-            case Scene.Title:
-                Debug.Log("タイトルシーン");
-                if (m_titleManger.GetIsStart())
-                {
-                    ChangeScene(Scene.InGame);
-                }
-                    break;
-            case Scene.InGame:
-                Debug.Log("インゲーム中");
+            case Scene.Title://タイトル
 
+                Debug.Log("タイトルシーンです");
+
+                //タイトルマネージャーからゲームスタートされてるかを取得して
+                //スタートされている&フェード中じゃなければゲームシーンに遷移する
+                if (m_titleManger.GetIsStart() && !m_fadeManager.m_isFading)
+                {
+                    StartCoroutine(ChangeInGame());
+                }
+                break;
+            case Scene.InGame:
+                Debug.Log("インゲームシーンです");
+                //TODO:Result遷移も同様の手順で行う
                 break;
             case Scene.Result:
-                Debug.Log("リザルトシーン");
-
+                Debug.Log("リザルトシーンです");
                 break;
         }
+    }
+
+    private IEnumerator ChangeInGame()
+    {
+        //まずフェードアウトを行う
+        yield return m_fadeManager.FadeOut(0.0f, 1.0f);
+
+        //暗転中にマネージャーを切り替え
+        ChangeScene(Scene.InGame);
+
+        //フェードインを行う
+        yield return m_fadeManager.FadeIn(1.0f, 0.0f);
     }
 
     public void ChangeScene(Scene scene)
     {
         m_scene = scene;
-        //対応したシーンのマネージャーのアクティブ化を行う
         switch (scene)
         {
             case Scene.Title:
                 m_titleManger.enabled = true;
-                m_inGameManger.enabled = false;
-                m_resultManger.enabled = false;
                 break;
             case Scene.InGame:
-                m_titleManger.enabled = false;
                 m_inGameManger.enabled = true;
-                m_resultManger.enabled = false;
                 break;
             case Scene.Result:
-                m_titleManger.enabled = false;
-                m_inGameManger.enabled = false;
                 m_resultManger.enabled = true;
                 break;
         }
--- BokoBokoRun/Assets/sakamoto/script/GameManagerBase.cs
+++ BokoBokoRun/Assets/sakamoto/script/GameManagerBase.cs
@@ -4,21 +4,12 @@ using UnityEngine;
 
 public class GameManagerBase : MonoBehaviour
 {
-    [SerializeField] protected FadeManager m_fadeManager;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         
-    }
-
-    protected virtual void OnEnable()
-    {
-        m_fadeManager.OnFadeIn();
-    }
-    protected virtual void OnDisable()
-    {
-        m_fadeManager.OnFadeOut();
     }
 
     // Update is called once per frame
