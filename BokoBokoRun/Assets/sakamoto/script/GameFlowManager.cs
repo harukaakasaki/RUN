e86b2810bc7ae4b5cdd268738f141c69f1385e89
ko1307 <208754810+13ko13@users.noreply.github.com>
2026-03-05 21:34:13 +0900

    セレクトマネージャーの準備

--- BokoBokoRun/Assets/sakamoto/script/GameFlowManager.cs
+++ BokoBokoRun/Assets/sakamoto/script/GameFlowManager.cs
@@ -9,6 +9,7 @@ public sealed class GameFlowManager : MonoBehaviour
     private static GameFlowManager m_instance;
 
     [SerializeField] private TitleManager m_titleManger;   //タイトルマネージャー
+    [SerializeField] private SelectManager m_selectManger;   //セレクトマネージャー
     [SerializeField] private InGameManager m_inGameManger;//インゲームマネージャー
     [SerializeField] private ResultManager m_resultManger;//リザルトマネージャー
     [SerializeField] private FadeManager m_fadeManager;//フェードマネージャー
@@ -25,6 +26,7 @@ public sealed class GameFlowManager : MonoBehaviour
     {
         None,   //何もない
         Title,  //タイトルシーン
+        Select, //セレクトシーン
         InGame, //ゲーム中
         Result, //リザルトシーン
     }
@@ -65,8 +67,9 @@ public sealed class GameFlowManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Test:パッドの数を取得
         m_padNum = Gamepad.all.Count;
-        Debug.Log("現在のコントローラーの数:" +  m_padNum);
+        Debug.Log("現在のコントローラーの数:" + GetPadNum());
 
         switch (m_scene)
         {
@@ -82,9 +85,14 @@ public sealed class GameFlowManager : MonoBehaviour
                 {
                     m_isTransitioning = true;
 
-                    StartCoroutine(ChangeInGame());
+                    StartCoroutine(ChangeSelect());
                 }
                 break;
+            case Scene.Select:
+                Debug.Log("セレクトシーンです");
+                //TODO:決定ボタンが押されたらゲームシーンに遷移する
+                break;
+
             case Scene.InGame:
                 Debug.Log("インゲームシーンです");
                 //TODO:Result遷移も同様の手順で行う
@@ -111,40 +119,71 @@ public sealed class GameFlowManager : MonoBehaviour
         //フェードインを行う
         yield return m_fadeManager.FadeIn(1.0f, 0.0f);
 
+        //シーン遷移中フラグを降ろす
+        m_isTransitioning = false;
+    }
+
+    private IEnumerator ChangeSelect()
+    {
+        //まずフェードアウトを行う
+        yield return m_fadeManager.FadeOut(0.0f, 1.0f);
+        //暗転中にセレクトマネージャーに切り替える
+        ChangeScene(Scene.Select);
+        //カメラも切り替える
+        //TODO:セレクトシーンカメラに切り替える
+
+        //カメラを完全に切り替えるために1フレーム松
+        yield return null;
+
+        //フェードインを行う
+        yield return m_fadeManager.FadeIn(1.0f, 0.0f);
+
+        //シーン遷移中フラグを降ろす
         m_isTransitioning = false;
     }
 
     public void ChangeScene(Scene scene)
     {
+        //シーンを切り替える
         m_scene = scene;
         switch (scene)
         {
-            case Scene.Title:
+            case Scene.Title://タイトル
+                //一旦すべてのマネージャーを非アクティブにする
                 AllEnabled();
+                //その後タイトルマネージャーのみをアクティブ化する
                 m_titleManger.enabled = true;
                 break;
-            case Scene.InGame:
+            case Scene.Select://セレクト
+                AllEnabled();
+                m_selectManger.enabled = true;
+                break;
+            case Scene.InGame://インゲーム
                 AllEnabled();
                 m_inGameManger.enabled = true;
                 break;
-            case Scene.Result:
+            case Scene.Result://リザルト
                 AllEnabled();
                 m_resultManger.enabled = true;
                 break;
         }
     }
 
+    /// <summary>
+    /// 全てのマネージャーを非アクティブ化する
+    /// </summary>
     private void AllEnabled()
     {
         m_titleManger.enabled = false;
+        m_selectManger.enabled = false;
         m_inGameManger.enabled = false;
         m_resultManger.enabled = false;
     }
     /// <summary>
     /// 現在接続されているコントローラーの数を返す関数
     /// </summary>
-    /// <returns></returns>
-    public int GetPadNum() 
+    /// <returns>コントローラーの数</returns>
+    public int GetPadNum()
     {
         return m_padNum;
     }
