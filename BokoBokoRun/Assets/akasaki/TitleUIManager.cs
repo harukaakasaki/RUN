50ed592709543aaed2cd456196bcf8d686956735
harukaakasaki <harukajobstar@gmail.com>
2026-03-05 20:38:56 +0900

    更新

--- BokoBokoRun/Assets/akasaki/TitleUIManager.cs
+++ BokoBokoRun/Assets/akasaki/TitleUIManager.cs
@@ -6,18 +6,24 @@ public class TitleUIManager : MonoBehaviour
 {
     public GameObject titleUI;
 
+    // タイトルUIを表示する
     public void Show()
     {
+        // UIオブジェクトをアクティブにして表示する
         titleUI.SetActive(true);
     }
 
+    // タイトルUIを非表示にする
     public void Hide()
     {
+        // UIオブジェクトを非アクティブにして非表示にする
         titleUI.SetActive(false);
     }
 
+    // ボタンが押されたときに呼ばれる
     public void OnStartButton()
     {
         Debug.Log("ゲームスタート");
+        // ここでUI切り替えなどを行う
     }
 }
