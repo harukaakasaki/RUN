da71879a0001cf7bfa855a0281c6fba3f48fb063
13ko13 <fko2547008@stu.o-hara.ac.jp>
2026-03-11 17:30:46 +0900

    セレクトシーン仮完成

--- BokoBokoRun/Assets/sakamoto/script/ResultManager.cs
+++ BokoBokoRun/Assets/sakamoto/script/ResultManager.cs
@@ -8,33 +8,55 @@ public class ResultManager : GameManagerBase
 {
     private bool m_isBackTitle = false;//タイトルに戻るか
 
+    //勝っているプレイヤーがいたときに使うもの
     private string m_bestPlayerTag;//一位になったプレイヤーのタグ
-    
+    [SerializeField] private GameObject[] m_winAnimPlayers;//勝った時のアニメーションを行っているプレイヤーの配列
+    [SerializeField] private GameObject m_winnerPosObj;//1位のプレイヤーの位置にあるオブジェクト
 
+    //全員が負けた時に使用するもの
+
+
+    //どちらの場合でも使う
     private bool m_isAllLoser = false;//全員が負けたかどうか
-
-    private GameObject[] m_players;//プレイヤーのオブジェクトを入れる配列
-
-    [SerializeField] private GameObject m_winnerPos;//1位のプレイヤーの位置にあるオブジェクト
-    [SerializeField] private GameObject[] m_loserPos;//2位以降のプレイヤーの位置にあるオブジェクト
+    [SerializeField] private GameObject[] m_loseAnimPlayers;//負けた時のアニメーションを行っているプレイヤーの配列
+    [SerializeField] private GameObject[] m_loserPosObj;//2位以降のプレイヤーの位置にあるオブジェクト
 
     private GameObject m_winnerPlayer;//1位になったプレイヤーのオブジェクト
- 
+
 
     private void OnEnable()
     {
-        //全プレイヤーから1位になったプレイヤーのオブジェクトを取得
-        for (int i = 0; i < m_players.Length; i++)
+        //勝者がいた場合の処理
+        if (!m_isAllLoser)
         {
-            if (m_players[i].tag == "testWinner")
+            //すべての勝ちアニメーションを持つオブジェクトのタグと
+            //勝ったプレイヤーのタグを照らし合わせる
+            for (int i = 0; i < m_winAnimPlayers.Length; i++)
             {
-                m_winnerPlayer = m_players[i];
+                //照らし合わせる
+                if (m_winAnimPlayers[i].tag == m_bestPlayerTag)
+                {
+                    //同じだったら配列に入っているオブジェクトの位置を
+                    //事前に設定されてある勝者の位置に移動させる
+                    m_winAnimPlayers[i].transform.position = m_winnerPosObj.transform.position;
+                }
+                else
+                {
+                    //それが負けたプレイヤーのタグだった場合
+                    m_loseAnimPlayers[i].transform.position = m_loserPosObj[i].transform.position;
+                }
             }
         }
-
-        //勝ったプレイヤーをwinnerPosに移動させる
-        m_winnerPlayer.transform.position = m_winnerPos.transform.position;
+        //すべてのプレイヤーが負けていた場合
+        else
+        {
+            for (int i = 0; i < m_loseAnimPlayers.Length; i++)
+            {
+                m_loseAnimPlayers[i].transform.position = m_loserPosObj[i].transform.position;
+            }
+        }
     }
+
 
     // Update is called once per frame
     void Update()
