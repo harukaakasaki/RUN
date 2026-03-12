6fd345e002b0970846c142e71052a48de5adca68
ひらお <fko2547051@stu.o-hara.ac.jp>
2026-03-12 14:48:24 +0900

    StageRoop完成

---
+++ BokoBokoRun/Assets/akasaki/hirao/MoveTransRoop.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTransRoop : MonoBehaviour
+{
+    [SerializeField] private Transform targetTransStart;//始点
+    [SerializeField] private Transform targetTransEnd;//終点
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.transform.position -= new Vector3(0.1f, 0, 0);
+
+        if(this.transform.position.x < targetTransEnd.position.x)
+        {
+            this.transform.position = new Vector3(targetTransStart.position.x, this.transform.position.y, this.transform.position.z);
+        }
+
+    }
+}
